a4551b343858bb3c9ce17483768c984f135a59bb
Paweł Ciosek <ciosek.pawel@gmail.com>
2022-02-01 20:51:44 +0100

    pavo

--- pages/pavo/index.js
+++ pages/pavo/index.js
@@ -4,7 +4,9 @@ import { AiFillAndroid, AiFillApple } from "react-icons/ai";
 const NavBar = (props) => {
     const [showBurgerMenu, setShowBurgerMenu] = useState(false);
     return (
-        <div className="p-4 fixed w-full bg-white">
+        <div
+            className="p-4 fixed w-full bg-white"
+        >
             <div className="container mx-auto max-w-screen-lg">
                 <div className="float-left pt-1">
                     <img className="w-32" src={"/pages/pavo/images/logo.svg"} />
@@ -46,19 +48,19 @@ const NavBar = (props) => {
     );
 };
 
-const MainBlock = (props) => {
+const Block1 = (props) => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 gab-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             <div>
-                <div className="text-center mb-5 md:mb-0 md:text-left md:mt-40 sm:mt-0">
-                    <h1 className="font-bold text-5xl mb-8">
+                <div className="text-center mb-16 md:mb-0 md:text-left md:mt-40 sm:mt-0">
+                    <h1 className="text-center font-bold text-5xl mb-8">
                         Team management mobile application
                     </h1>
-                    <p className="mt-3 text-gray-700 mb-8">
+                    <p className="text-center mt-3 text-gray-700 mb-8">
                         Start getting things done together with your team based
                         on Pavo's revolutionary team management features
                     </p>
-                    <div className="mt-3">
+                    <div className="mt-3 text-center">
                         <button className="bg-red-700 p-3 mr-5 text-white rounded-xl">
                             <AiFillAndroid className="inline" /> Download
                         </button>
@@ -68,19 +70,14 @@ const MainBlock = (props) => {
                     </div>
                 </div>
             </div>
-            <div>
-                <img
-                    className="m-auto"
-                    src="/pages/pavo/images/header-smartphone.png"
-                />
-            </div>
+            <div className="bg-[url('/pages/pavo/images/header-smartphone.png')] bg-center bg-no-repeat bg-auto h-[600px]"></div>
         </div>
     );
 };
 
-const TextBlock = (props) => {
+const Block2 = (props) => {
     return (
-        <div className="text-center text-3xl text-gray-800">
+        <div className="text-center text-3xl">
             <p>
                 Team management mobile apps don’t get better than Pavo. It’s
                 probably the best app in the world for this purpose. Don’t
@@ -91,29 +88,85 @@ const TextBlock = (props) => {
     );
 };
 
+const Block3 = (props) => {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            <div className="lp-block2-card">
+                <div className="bg-[url('/pages/pavo/images/features-icon-1.svg')] lp-block2-cover"></div>
+                <div className="lp-block2-title">Platform Integration</div>
+                <div className="lp-block2-subtitle">
+                    You sales force can use the app on any smartphone platform
+                    without compatibility issues
+                </div>
+            </div>
+            <div className="lp-block2-card">
+                <div className="bg-[url('/pages/pavo/images/features-icon-2.svg')] lp-block2-cover"></div>
+                <div className="lp-block2-title">Easy On Resources</div>
+                <div className="lp-block2-subtitle">
+                    Works smoothly even on older generation hardware due to our
+                    optimization efforts
+                </div>
+            </div>
+            <div className="lp-block2-card">
+                <div className="bg-[url('/pages/pavo/images/features-icon-3.svg')] lp-block2-cover"></div>
+                <div className="lp-block2-title">Great Performance</div>
+                <div className="lp-block2-subtitle">
+                    Optimized code and innovative technology insure no delays
+                    and ultra-fast responsiveness
+                </div>
+            </div>
+            <div className="lp-block2-card">
+                <div className="bg-[url('/pages/pavo/images/features-icon-4.svg')] lp-block2-cover"></div>
+                <div className="lp-block2-title">Multiple Languages</div>
+                <div className="lp-block2-subtitle">
+                    Choose from one of the 40 languages that come pre-installed
+                    and start selling smarter
+                </div>
+            </div>
+            <div className="lp-block2-card">
+                <div className="bg-[url('/pages/pavo/images/features-icon-5.svg')] lp-block2-cover"></div>
+                <div className="lp-block2-title">Free Updates</div>
+                <div className="lp-block2-subtitle">
+                    Don't worry about future costs, pay once and receive all
+                    future updates at no extra cost
+                </div>
+            </div>
+            <div className="lp-block2-card">
+                <div className="bg-[url('/pages/pavo/images/features-icon-6.svg')] lp-block2-cover"></div>
+                <div className="lp-block2-title">Community Support</div>
+                <div className="lp-block2-subtitle">
+                    Register the app and get acces to knowledge and ideas from
+                    the Pavo online community
+                </div>
+            </div>
+        </div>
+    );
+};
+
 const Pavo = () => {
     return (
         <div
-            style={{
-                background:
-                    "linear-gradient(rgba(197, 234, 249, 1), rgba(255, 255, 255, 1))",
-            }}
+        // className="bg-gradient-to-r from-cyan-500 to-blue-500 bg-no-repeat bg-auto"
+        style={{
+            background:
+                "linear-gradient(rgba(197, 234, 249, 1), rgba(255, 255, 255, 1))"
+        }}
         >
             <div className="h-16">
                 <NavBar />
             </div>
             <div className="container max-w-screen-lg mx-auto p-4">
-                <div className="mt-32 mb-32">
-                    <MainBlock />
+                <div className="lp-block">
+                    <Block1 />
                 </div>
-                <div className="mt-32 mb-32">
-                    <TextBlock />
+                <div className="lp-block">
+                    <Block2 />
                 </div>
-                <div className="mt-5">
-                    <MainBlock />
+                <div className="lp-block">
+                    <Block3 />
                 </div>
-                <div className="mt-5">
-                    <MainBlock />
+                <div className="lp-block">
+                    <Block1 />
                 </div>
             </div>
         </div>
